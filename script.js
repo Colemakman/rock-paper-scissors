88cd77c944fa227692f2c0a9ea44d3cbc8292606
Alfie <34726358+ToMuchTNT@users.noreply.github.com>
2023-12-01 01:18:26 +0000

    Created getComputerChoice() and playRound()

---
+++ script.js
@@ -0,0 +1,55 @@
+
+function getComputerChoice() {
+
+    let choice;
+    let num = Math.floor(Math.random()* 3) + 1
+    switch(num) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection)  {
+
+    console.log(computerSelection)
+
+    lossMessage = "You lose"
+    winMessage = "You win"
+
+    playerSelection = playerSelection.toLowerCase()
+
+    if (playerSelection === computerSelection) {
+        return "Draw"
+    }
+    
+    switch(playerSelection) {
+        case "rock": {
+            switch(computerSelection) {
+                case "paper":
+                    return lossMessage
+                case "scissors":
+                    return winMessage
+            }
+        }
+        case "paper": {
+            switch(computerSelection) {
+                case "rock":
+                    return winMessage
+                case "scissors":
+                    return lossMessage
+            }
+        }
+        case "scissors": {
+            switch(computerSelection) {
+                case "rock":
+                    return lossMessage
+                case "paper":
+                    return winMessage
+            }
+        }
+    }
+}
